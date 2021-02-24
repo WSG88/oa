86c37daab7c261a9f2e5dd85b6d3169c5c214e14
WANGSIGANG <513969457@qq.com>
2021-02-25 07:50:01 +0800

    20210225 不计算夜班

--- src/main/java/RoomAgain.java
+++ src/main/java/RoomAgain.java
@@ -14,9 +14,9 @@ public class RoomAgain {
     public static void main(String[] args) throws Exception {
         Utils.clear();
         Utils.clearList();
-        Utils.YEAR_MONTH = "202010";
+        Utils.YEAR_MONTH = "202101";
         Utils.ROOM = 1;
-        Utils.FILE_NAME = Utils.YEAR_MONTH + "__" + Utils.ROOM + "车间1.xlsx";
+        Utils.FILE_NAME = "202101_1车间1.xlsx";
         List<Data> dataArrayList = new ArrayList<>();
         Workbook wbs = Utils.getWorkbook();
         Sheet childSheet = wbs.getSheetAt(0);
@@ -47,47 +47,47 @@ public class RoomAgain {
                         listNew.add(s);
                     }
                 }
-                //处理夜班数据
-                long l2 = DateUtil.parse(date + "08:06", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                long l3 = DateUtil.parse(date + "19:00", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                long l4 = DateUtil.parse(date + "20:06", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                long l5 = DateUtil.parse(date + "24:00", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                if (listNew.size() == 1 || listNew.size() == 2) {
-                    if (listNew.size() == 1) {
-                        String str0 = listNew.get(0);
-                        long l0 = DateUtil.parse(date + str0, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                        if (l0 < l2) {
-                            listNew.clear();
-                            listNew.add("00:00");
-                            listNew.add(str0);
-                        } else if (l0 < l4 && l0 > l3) {
-                            listNew.clear();
-                            listNew.add(str0);
-                            listNew.add("24:00");
-                        } else {
-                            //System.out.println(name + date + listNew);
-                        }
-                    }
-                    if (listNew.size() == 2) {
-                        String str0 = listNew.get(0);
-                        String str1 = listNew.get(1);
-                        long l0 = DateUtil.parse(date + str0, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                        long l1 = DateUtil.parse(date + str1, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
-                        if (l0 < l2 && ((l1 < l4 && l1 > l3) || (l1 <= l5 && l1 > l3))) {
-                            listNew.clear();
-                            listNew.add("00:00");
-                            listNew.add(str0);
-                            listNew.add(str1);
-                            listNew.add("24:00");
-                        } else if (l0 > l3 && l1 <= l5 && l1 > l3) {
-
-                        } else if (l1 < l2) {
-
-                        } else {
-                            //System.out.println(name + date + listNew);
-                        }
-                    }
-                }
+//                //处理夜班数据
+//                long l2 = DateUtil.parse(date + "08:06", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                long l3 = DateUtil.parse(date + "19:00", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                long l4 = DateUtil.parse(date + "20:06", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                long l5 = DateUtil.parse(date + "24:00", "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                if (listNew.size() == 1 || listNew.size() == 2) {
+//                    if (listNew.size() == 1) {
+//                        String str0 = listNew.get(0);
+//                        long l0 = DateUtil.parse(date + str0, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                        if (l0 < l2) {
+//                            listNew.clear();
+//                            listNew.add("00:00");
+//                            listNew.add(str0);
+//                        } else if (l0 < l4 && l0 > l3) {
+//                            listNew.clear();
+//                            listNew.add(str0);
+//                            listNew.add("24:00");
+//                        } else {
+//                            //System.out.println(name + date + listNew);
+//                        }
+//                    }
+//                    if (listNew.size() == 2) {
+//                        String str0 = listNew.get(0);
+//                        String str1 = listNew.get(1);
+//                        long l0 = DateUtil.parse(date + str0, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                        long l1 = DateUtil.parse(date + str1, "yyyyMMddHH:mm").toCalendar().getTimeInMillis();
+//                        if (l0 < l2 && ((l1 < l4 && l1 > l3) || (l1 <= l5 && l1 > l3))) {
+//                            listNew.clear();
+//                            listNew.add("00:00");
+//                            listNew.add(str0);
+//                            listNew.add(str1);
+//                            listNew.add("24:00");
+//                        } else if (l0 > l3 && l1 <= l5 && l1 > l3) {
+//
+//                        } else if (l1 < l2) {
+//
+//                        } else {
+//                            //System.out.println(name + date + listNew);
+//                        }
+//                    }
+//                }
 
                 if (!listNew.isEmpty()) {
                     //补全考勤数据
